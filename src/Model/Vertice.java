4c7a2ed1753696958c027d0b2f0164c17e44ca95
Adryel <Adryel@Adryel-pc>
2020-03-18 20:14:42 -0300

    Primeiro comit

--- src/Model/Vertice.java
+++ src/Model/Vertice.java
@@ -9,21 +9,35 @@ public class Vertice {
     private int distanciaOrigem;
     private Vertice antecessor;
     private boolean terminal;
-
+    
+    // Método Construtor 
     public Vertice(String nome, boolean terminal) {
         this.nome = nome;
         this.arestas = new LinkedList();
         this.terminal = terminal;
     }
-
+     /** 
+      * Método para adicionar uma nova aresta a dois vertices
+      * @param destino Verticie de destino o qual será o fim da aresta
+      * @param peso Peso da aresta
+      */
+   
     public boolean addAresta(Vertice destino, int peso) {
         return arestas.add(new Aresta(peso, destino));
     }
-
+   
+    // Método que retorna a lista de aresta do vertice
     public LinkedList<Aresta> getArestas() {
         return this.arestas;
     }
-
+    
+    /**
+     * Método que retorna o peso de uma aresta com base
+     * em um vertice adjacente
+     * @param adjacente Vertice adjacente de outro vertice
+     * @return Retorna o peso da aresta
+     */
+    
     public int getPesoLigacaoCom(Vertice adjacente) {
         for (int i = 0; i < arestas.size(); i++) {
             Aresta arestaAtual = arestas.get(i);
@@ -32,33 +46,40 @@ public class Vertice {
                 return arestaAtual.getPeso();
             }
         }
-        return -1;
+        return -1; // O Método retorna -1 caso não haja ligação
     }
-
+   
+    // Método retorna o nome do vertice
     public String getNome() {
         return nome;
     }
-
+    
+    // Método que altera a distância de origem do vertice
     public void setDistanciaOrigem(int novaDistanciaOrigem) {
         this.distanciaOrigem = novaDistanciaOrigem;
     }
-
+    // Método que retorna a distancia da origem do vertice
     public int getDistanciaOrigem() {
         return this.distanciaOrigem;
     }
-
+    // Método que altera o vertice antecessor de um vertice
     public void setVerticeAntecessor(Vertice novoAntecessor) {
         this.antecessor = novoAntecessor;
     }
-
+    // Método que retorna vertice antecessor de um vertice
     public Vertice getVerticeAntecessor() {
         return this.antecessor;
     }
-
+    // Método que retorna se o vertice é terminal ou não
     public boolean isTerminal() {
         return this.terminal;
     }
-
+    /** 
+     * Método que remove aresta entre dois vertices
+     * @param emRemocao Vertice o qual será removido a ligação
+     * @return Retorna true ou false indicando se a operação
+     * foi bem sucedida ou não
+     */
     public boolean removeLigacaoCom(Vertice emRemocao) {
         for (int i = 0; i < arestas.size(); i++) {
             Aresta arestaAtual = arestas.get(i);
@@ -67,6 +88,6 @@ public class Vertice {
                 return arestas.remove(arestaAtual);
             }
         }
-        return false;
+        return false; // retorna false caso não haja ligação
     }
 }
