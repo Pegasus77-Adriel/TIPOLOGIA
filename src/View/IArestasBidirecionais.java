1468ab00fa22a7d769a9c49d9f47901476eca7b3
Marcos <Marcos@Marcos-PC>
2020-03-18 13:53:49 -0300

    P.B.L. Edição final do projeto

--- src/View/IArestasBidirecionais.java
+++ src/View/IArestasBidirecionais.java
@@ -3,23 +3,33 @@ package View;
 import com.mxgraph.util.mxConstants;
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxStylesheet;
-import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * Classe responsável por definir a aparência das arestas na interface gráfica
+ *
+ * @author Marcos
+ */
 public class IArestasBidirecionais {
 
+    /**
+     * Prepara as configurações de estilo para as arestas sempre que uma nova
+     * instância for chamada.
+     *
+     * @param grafo Componente de interface gráfica cujas arestas terão seu
+     * estilo alterado
+     */
     public IArestasBidirecionais(mxGraph grafo) {
         mxStylesheet stylesheet = grafo.getStylesheet();
-        String nomeEstilo = "ArestasBidirecionais";
-        HashMap<String, Object> estilo = new HashMap();
-        estilo.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_CONNECTOR);
-        estilo.put(mxConstants.STYLE_STARTARROW, mxConstants.NONE);
-        estilo.put(mxConstants.STYLE_STARTARROW, mxConstants.NONE);
-        estilo.put(mxConstants.STYLE_ENDARROW, mxConstants.NONE);
-        estilo.put(mxConstants.STYLE_MOVABLE, false);
-        //estilo.put(mxConstants.STYLE_DASHED, true); (IGNORE)
-        estilo.put(mxConstants.STYLE_VERTICAL_LABEL_POSITION, mxConstants.ALIGN_BOTTOM);
-        estilo.put(mxConstants.STYLE_EDITABLE, false);
-        
-        stylesheet.putCellStyle(nomeEstilo, estilo);
+        Map<String, Object> estiloPadrao = (Map<String, Object>) stylesheet.getDefaultEdgeStyle();
+        estiloPadrao.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_CONNECTOR);
+        estiloPadrao.put(mxConstants.STYLE_STARTARROW, mxConstants.NONE);
+        estiloPadrao.put(mxConstants.STYLE_STARTARROW, mxConstants.NONE);
+        estiloPadrao.put(mxConstants.STYLE_ENDARROW, mxConstants.NONE);
+        estiloPadrao.put(mxConstants.STYLE_MOVABLE, false);
+        estiloPadrao.put(mxConstants.STYLE_VERTICAL_LABEL_POSITION, mxConstants.ALIGN_BOTTOM);
+        estiloPadrao.put(mxConstants.STYLE_EDITABLE, false);
+
+        stylesheet.setDefaultEdgeStyle(estiloPadrao);
     }
 }
